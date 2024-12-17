f4a011783563ae43a3994b81062172b4b346be9e
xs-es <xses.echo@gmail.com>
2024-12-16 22:46:55 -0500

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,5 @@
+document.getElementById('animateButton').addEventListener('click', function() {
+    alert('Button clicked! This is where animations can be added.');
+    // Add animation code here
+});
+
